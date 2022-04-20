63f17098ba4258ad1a55a6dc043f2a57c606f732
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-04-20 08:24:16 +0700

    Feature - #27 - Create Button component

--- practice-one/.storybook/main.js
+++ practice-one/.storybook/main.js
@@ -1,12 +1,18 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })];
+    return config;
+  },
+  staticDirs: ['../src/assets/images'],
+};
---
+++ practice-one/src/components/buttons/button.stories.tsx
@@ -0,0 +1,33 @@
+import { Story } from '@storybook/react';
+import { Button, ButtonProp } from '@components/buttons/index';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template: Story<ButtonProp> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  color: 'primary',
+  children: 'Button',
+  extraClass: 'btn btn-outline-primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: 'secondary',
+  children: 'Button',
+  extraClass: 'btn btn-secondary',
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+  color: 'outline-green',
+  children: 'Button',
+  extraClass: 'btn-standard btn-content',
+};
