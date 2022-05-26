a386c0d7b55c287d1285a039d458207d791a35bd
Nhung Le <nhung.le@asnet.com.vn>
2022-05-26 10:37:33 +0700

    Revert "Merge branch 'feature/add-book' into 'practice-two'"
    This reverts merge request !26

--- practice-two/src/components/header/index.tsx
+++ practice-two/src/components/header/index.tsx
@@ -1,41 +1,15 @@
 import { Button } from '@components/common/button';
-import { Popup } from '@components/common/popup';
-import { Form } from '@components/form';
-import { Book } from '@interface/book';
 import './header.css';
 
-type HeaderProp = {
-  isOpen: boolean;
-  onHandleClose: () => void;
-  onHandleCreate: (book: Book) => void;
-};
-
-export const Header = (props: HeaderProp): JSX.Element => {
-  const { isOpen, onHandleClose, onHandleCreate } = props;
-
+export const Header = (): JSX.Element => {
   return (
     <header className="page-header">
       <h2 className="heading">A list of books</h2>
       <div className="nav-btn">
-        <Button
-          handleClick={onHandleClose}
-          color="btn-primary"
-          size="btn-large"
-          typeButton="button"
-        >
+        <Button color="btn-primary" size="btn-large" typeButton="button">
           Add New Book
         </Button>
       </div>
-      {isOpen && (
-        <Popup handleClose={onHandleClose}>
-          <Form
-            labelButton="Create"
-            formTitle="Create New Book"
-            onCreate={onHandleCreate}
-            onHandleClose={onHandleClose}
-          />
-        </Popup>
-      )}
     </header>
   );
 };
