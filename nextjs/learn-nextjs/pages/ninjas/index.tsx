301e805a273d367bd7f8837bcec879cb98a77ceb
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-10-27 17:58:52 +0700

    feat: create examples for nextjs

---
+++ nextjs/learn-nextjs/pages/ninjas/index.tsx
@@ -0,0 +1,33 @@
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
+import { INinjas } from '../../types'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data: INinjas[] = await res.json()
+
+  return {
+    props: { ninjas: data },
+  }
+}
+
+const Ninjas = ({ ninjas }: { ninjas: INinjas[] }) => {
+  console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja: INinjas) => (
+        <Link
+          className={styles.single}
+          href={'/ninjas/' + ninja.id}
+          key={ninja.id}
+        >
+          {ninja.name}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Ninjas
