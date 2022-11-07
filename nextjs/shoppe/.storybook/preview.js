a4a4f9cb90ab30961d2b785c3cba8ebf5fd270e9
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-11-07 11:23:39 +0700

    fix: fix import path alias for storybook

--- nextjs/shoppe/.storybook/preview.js
+++ nextjs/shoppe/.storybook/preview.js
@@ -1,5 +1,10 @@
+import { customTheme } from '@themes/index'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  chakra: {
+    theme: customTheme,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
