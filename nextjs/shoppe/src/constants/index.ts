1e3a49154c60906ca563a1eb5a19815a00ad3a52
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-11-09 02:21:36 +0700

    feat: create navigation component #159

---
+++ nextjs/shoppe/src/constants/index.ts
@@ -0,0 +1,17 @@
+export const NAV_LIST = [
+  {
+    value: 1,
+    name: 'Contact',
+    href: '/contact',
+  },
+  {
+    value: 2,
+    name: 'Terms of services',
+    href: '/terms-of-services',
+  },
+  {
+    value: 3,
+    name: 'Shipping and Return',
+    href: '/shipping-and-return',
+  },
+]
