2844bdd13b75c04c4504c77559a8c0ff23e33678
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-11-10 16:27:51 +0700

    feat: implement list page ui #165

--- nextjs/shoppe/src/pages/_app.tsx
+++ nextjs/shoppe/src/pages/_app.tsx
@@ -1,14 +1,21 @@
+import '../../styles/reset.css'
 import '../../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 // Themes
 import { customTheme } from '@themes/index'
 
+// Layouts
+import PageLayout from '@layouts/PageLayout'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
-      <Component {...pageProps} />
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
     </ChakraProvider>
   )
 }
--- nextjs/shoppe/src/pages/index.tsx
+++ nextjs/shoppe/src/pages/index.tsx
@@ -1,5 +1,73 @@
+import { Box, Flex, Heading } from '@chakra-ui/react'
+
+// Components
+import Button from '@components/Button'
+import CardProduct from '@components/CardProduct'
+import Search from '@components/Search'
+
+// Mocks
+import { LIST_PRODUCT } from '@mocks/mockData'
+
+// Types
+import { IProduct } from '@self-types/index'
+
 const Home = () => {
-  return <div>Home Page</div>
+  // TODO:
+  // - Replace mocks data with fetched data, will be done in integration step
+  const products: IProduct[] = LIST_PRODUCT
+
+  // Handle change search
+  const handleOnChangeSearch = () => {
+    // Handle change
+  }
+
+  // Handle search when user press enter
+  const handleKeyDown = () => {
+    // Handle search
+  }
+
+  // Handle search when user press submit button
+  const handleSubmitSearch = () => {
+    // Handle search
+  }
+
+  // Handle load more
+  const handleLoadMore = () => {
+    // Handle load
+  }
+
+  return (
+    <Box pt="96px">
+      <Heading as="h2" fontSize="large" fontWeight="medium">
+        Shop The Latest
+      </Heading>
+      <Flex pt="40px" gap="35px" justifyContent="space-between">
+        <Box minW="261px">
+          <Search
+            onChange={handleOnChangeSearch}
+            onKeyDown={handleKeyDown}
+            onClick={handleSubmitSearch}
+            placeholder="Search..."
+          />
+        </Box>
+        <Flex flexDirection="column" justifyContent="center">
+          <Flex flexWrap="wrap" gap="24px" paddingBottom="60px">
+            {products.map((product) => (
+              <CardProduct key={`product-${product.id}`} product={product} />
+            ))}
+          </Flex>
+          <Button
+            onClick={handleLoadMore}
+            margin="0 auto"
+            variant="primary"
+            size="medium"
+          >
+            Load More
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  )
 }
 
 export default Home
