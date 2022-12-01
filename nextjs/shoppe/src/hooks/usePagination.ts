791cae2458410d3412fcb3cb69b3327867d95b1b
Nhan Tran Quy Le <nhan.le@asnet.com.vn>
2022-12-01 17:56:05 +0700

    refactor: refactor usePagination

--- nextjs/shoppe/src/hooks/usePagination.ts
+++ nextjs/shoppe/src/hooks/usePagination.ts
@@ -1,26 +1,23 @@
 import useSWRInfinite from 'swr/infinite'
 
 export const usePagination = <T>(url: string) => {
-  const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
+  const { data, error, size, setSize } = useSWRInfinite(
     (index) => `${url}page=${index + 1}&limit=6`,
   )
 
   const paginatedData = data && (data.flat() as T)
-  const isLoading = !data && !error
   const isLoadingMore =
-    isLoading || (size > 0 && data && typeof data[size - 1] === 'undefined')
-  const isEmpty = data?.[0]?.length === 0
-  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 6)
+    (!data && !error) ||
+    (size > 0 && data && typeof data[size - 1] === 'undefined')
+  const isReachingEnd =
+    data?.length === 0 || (data && data[data.length - 1]?.length < 6)
 
   return {
     paginatedData,
     error,
     isLoadingMore,
-    isEmpty,
     size,
     setSize,
-    mutate,
-    isValidating,
     isReachingEnd,
   }
 }
